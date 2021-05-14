9e2c66ce188656514169d3f435073d811225e023
gustasDesktop <gustaDesktop@github.com>
2021-05-14 14:51:50 -0300

    Created pokemonList (first locally, but I intend to extract it from a db soon).

--- src/app/views/list/list.component.ts
+++ src/app/views/list/list.component.ts
@@ -7,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
+  pokemonList = [
+    { name: "Bulbasaur", number: 1 },
+    { name: "Ivysaur", number: 2 },
+    { name: "Venusaur", number: 3 },
+    { name: "Charmander", number: 4 },
+    { name: "Charmeleon", number: 5 },
+    { name: "Charizard", number: 6 },
+    { name: "Squirtle", number: 7 },
+    { name: "Wartortle", number: 8 },
+    { name: "Blastoise", number: 9 }
+  ]
+  
   isVisible = true;
   
   student = {
