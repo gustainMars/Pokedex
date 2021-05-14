a0fd72156e1acfa083f2fda81749a3dcfe7cc793
gustasDesktop <gustaDesktop@github.com>
2021-05-14 15:33:12 -0300

    Added Pokemon Types! ^^ (In type2 I did a ngIf to check if it's null, to avoid errors in the browser console)

--- src/app/views/list/list.component.ts
+++ src/app/views/list/list.component.ts
@@ -8,15 +8,15 @@ import { Component, OnInit } from '@angular/core';
 export class ListComponent implements OnInit {
 
   pokemonList = [
-    { name: "Bulbasaur", number: 1 },
-    { name: "Ivysaur", number: 2 },
-    { name: "Venusaur", number: 3 },
-    { name: "Charmander", number: 4 },
-    { name: "Charmeleon", number: 5 },
-    { name: "Charizard", number: 6 },
-    { name: "Squirtle", number: 7 },
-    { name: "Wartortle", number: 8 },
-    { name: "Blastoise", number: 9 }
+    { name: "Bulbasaur", number: 1, type1: "grass", type2: "poison" },
+    { name: "Ivysaur", number: 2, type1: "grass", type2: "poison" },
+    { name: "Venusaur", number: 3, type1: "grass", type2: "poison" },
+    { name: "Charmander", number: 4, type1: "fire", type2: "" },
+    { name: "Charmeleon", number: 5, type1: "fire", type2: "" },
+    { name: "Charizard", number: 6, type1: "fire", type2: "flying" },
+    { name: "Squirtle", number: 7, type1: "water", type2: "" },
+    { name: "Wartortle", number: 8, type1: "water", type2: "" },
+    { name: "Blastoise", number: 9, type1: "water", type2: "" }
   ]
   
   isVisible = true;
