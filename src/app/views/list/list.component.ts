b4d1ae9848aded3f77228039c1290a7f3cb43ad0
gustasDesktop <gustaDesktop@github.com>
2021-05-23 17:46:45 -0300

    Changed to single quotes ^^', Added pkmSprite to load Pokemon img from serebii, added selectPkm to change selectedPkm's content.

--- src/app/views/list/list.component.ts
+++ src/app/views/list/list.component.ts
@@ -7,32 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
+  selectedPkm: any;
   pokemonList = [
-    { name: "Bulbasaur", number: 1, type1: "grass", type2: "poison" },
-    { name: "Ivysaur", number: 2, type1: "grass", type2: "poison" },
-    { name: "Venusaur", number: 3, type1: "grass", type2: "poison" },
-    { name: "Charmander", number: 4, type1: "fire", type2: "" },
-    { name: "Charmeleon", number: 5, type1: "fire", type2: "" },
-    { name: "Charizard", number: 6, type1: "fire", type2: "flying" },
-    { name: "Squirtle", number: 7, type1: "water", type2: "" },
-    { name: "Wartortle", number: 8, type1: "water", type2: "" },
-    { name: "Blastoise", number: 9, type1: "water", type2: "" }
-  ]
-  
-  isVisible = true;
-  
-  student = {
-    data : {
-      name : "Ash"
-    }
+    { name: 'Bulbasaur', number: 1, type1: 'grass', type2: 'poison' },
+    { name: 'Ivysaur', number: 2, type1: 'grass', type2: 'poison' },
+    { name: 'Venusaur', number: 3, type1: 'grass', type2: 'poison' },
+    { name: 'Charmander', number: 4, type1: 'fire', type2: '' },
+    { name: 'Charmeleon', number: 5, type1: 'fire', type2: '' },
+    { name: 'Charizard', number: 6, type1: 'fire', type2: 'flying' },
+    { name: 'Squirtle', number: 7, type1: 'water', type2: '' },
+    { name: 'Wartortle', number: 8, type1: 'water', type2: '' },
+    { name: 'Blastoise', number: 9, type1: 'water', type2: '' }
+  ];
+
+  get pkmSprite(): string {
+    const number1 = ('000' + this?.selectedPkm?.number).slice(-3);
+    return `//serebii.net/sunmoon/pokemon/${number1}.png`;
   }
-
-  myList = [1, 2, 3, 4, 5];
-  value = 5;
-
   constructor() { }
 
   ngOnInit(): void {
+  }
+  selectPkm(pkm: any): void {
+    this.selectedPkm = pkm;
   }
 
 }
