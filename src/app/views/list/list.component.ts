2b8cac1ec5ceec188d77abbc4dd3c063f31a30c7
Gustavo Marques <gmartes23@gmail.com>
2021-06-27 20:29:44 -0300

    Added nameFilter search using List from pokeapi

--- src/app/views/list/list.component.ts
+++ src/app/views/list/list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PokeapiService } from 'src/app/services/pokeapi.service';
 
 @Component({
   selector: 'app-list',
@@ -7,24 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
+  nameFilter = '';
   selectedPkm: any;
-  pokemonList = [
-    { name: 'Bulbasaur', number: 1, type1: 'grass', type2: 'poison' },
-    { name: 'Ivysaur', number: 2, type1: 'grass', type2: 'poison' },
-    { name: 'Venusaur', number: 3, type1: 'grass', type2: 'poison' },
-    { name: 'Charmander', number: 4, type1: 'fire', type2: '' },
-    { name: 'Charmeleon', number: 5, type1: 'fire', type2: '' },
-    { name: 'Charizard', number: 6, type1: 'fire', type2: 'flying' },
-    { name: 'Squirtle', number: 7, type1: 'water', type2: '' },
-    { name: 'Wartortle', number: 8, type1: 'water', type2: '' },
-    { name: 'Blastoise', number: 9, type1: 'water', type2: '' }
-  ];
+
+  // tslint:disable-next-line: typedef
+  get pokemonList(){
+    return this.pokeapi.pokemonList.filter(pokemon => {
+      return pokemon.name.toLowerCase().indexOf(this.nameFilter.toLowerCase()) !== -1;
+    });
+  }
 
   get pkmSprite(): string {
     const number1 = ('000' + this?.selectedPkm?.number).slice(-3);
     return `//serebii.net/sunmoon/pokemon/${number1}.png`;
   }
-  constructor() { }
+  constructor(
+    private pokeapi: PokeapiService
+    ) { }
 
   ngOnInit(): void {
   }
