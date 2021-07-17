3cb87e203b353e14389eb1e57f237108d3ec4eb0
Gustavo Marques <gmartes23@gmail.com>
2021-07-17 16:16:32 -0300

    call pokeapi.listAll + fixed some typos

--- src/app/views/list/list.component.ts
+++ src/app/views/list/list.component.ts
@@ -13,22 +13,24 @@ export class ListComponent implements OnInit {
 
   // tslint:disable-next-line: typedef
   get pokemonList(){
-    return this.pokeapi.pokemonList.filter(pokemon => {
+    return this.pokeapi.pokemonList.filter((pokemon: any) => {
       return pokemon.name.toLowerCase().indexOf(this.nameFilter.toLowerCase()) !== -1;
     });
   }
 
   get pkmSprite(): string {
-    const number1 = ('000' + this?.selectedPkm?.number).slice(-3);
-    return `//serebii.net/sunmoon/pokemon/${number1}.png`;
+    const pokeNumber = ('000' + this?.selectedPkm?.number).slice(-3);
+    return `//serebii.net/sunmoon/pokemon/${pokeNumber}.png`;
   }
   constructor(
     private pokeapi: PokeapiService
     ) { }
 
   ngOnInit(): void {
+    this.pokeapi.listAll();
   }
-  selectPkm(pkm: any): void {
+
+  selectPkm(pkm): void {
     this.selectedPkm = pkm;
   }
 
